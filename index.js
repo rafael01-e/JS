86dcca14b76d03684413331af5ac03b265cab1a3
Rafael Ferreira Vasconcelos <rafaelferreiravasconcelos20@gmail.com>
2026-07-07 10:12:32 -0300

    Aula 1

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log(`Hello, World!`);
+console.log(`I'm learning JS`)
+
+//window.alert(`This is an alert`)
+//window.alert(`Learning HTML is fun`)
+
+document.getElementById("myH1").textContent = 'Learning JS';
+document.getElementById("myP").textContent = `I'am trying to learn a bunch of tools, so i can get a job`
+
