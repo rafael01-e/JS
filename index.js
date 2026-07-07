330e5816f4605db0829759bd9b7d1df583e88c9c
Rafael Ferreira Vasconcelos <rafaelferreiravasconcelos20@gmail.com>
2026-07-07 15:55:54 -0300

    Aula 10 - If statements

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-//Random Numbers --> Using dice as exemple
-const max = 6
-const min = 1
-let diceNumber;
+// If statements
+let age;
 
-document.getElementById("roller").onclick = function () {
-    diceNumber = Math.floor(Math.random() * max) + min;
-    document.getElementById("result").textContent = diceNumber;
+document.getElementById("mySubmit").onclick = function () {
+    age = document.getElementById("ageText").value;
+    age = Number(age);
+    if (age >= 18) {
+        document.getElementById("result").textContent = "You are old enough to get a license";
+    }
+    else if (age < 18 && age > 0) {
+        document.getElementById("result").textContent = "You are too young to get a license";
+    }
+    else {
+        document.getElementById("result").textContent = "Enter a valid age";
+    }
 }
