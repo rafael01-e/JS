01c21b42e24e67ac647db183ad0132e176519f5a
Rafael Ferreira Vasconcelos <rafaelferreiravasconcelos20@gmail.com>
2026-07-07 10:41:36 -0300

    Aula 3 - Operadores aritméticos

--- index.js
+++ index.js
@@ -1,19 +1,9 @@
-let x = 100.00;
-let firstname = "Rafael";
+let students = 30
 
-document.getElementById("myH1").textContent = `Hello, ${firstname}! Your balance is: $${x}`;
-document.getElementById("myP").textContent = `Type of balance is: ${typeof x}`;
+// students = students + 1
+// students = students - 1
+// students = students / 2
+// students = students ** 2
+students = students % 2
 
-let online = true;
-console.log(typeof online);
-console.log(`${firstname} is online: ${online}`);
-
-let name = "Rafael Marcellini Oliviano"
-let birthdate = `21/03/2007`
-let father = `Francesco Marcellini Oliviano`
-let mother = `Silvia Maria Marcellini Oliviano`
-
-document.getElementById("p1").textContent = `Name: ${name}`;
-document.getElementById("p2").textContent = `Birthdate: ${birthdate}`;
-document.getElementById("p3").textContent = `Father: ${father}`;
-document.getElementById("p4").textContent = `Mother: ${mother}`;
+console.log(students)
