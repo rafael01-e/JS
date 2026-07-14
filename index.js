543e091b9abd2c0ae6da0cb1defd7890ee5b86a3
Rafael Ferreira Vasconcelos <rafaelferreiravasconcelos20@gmail.com>
2026-07-14 15:47:32 -0300

    String slicing

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
-let username = "RafaelFerreiraVasconcelos";
-let phonenumber = "(83) 99133-6039"
+const full_name = "João Viscente Pagliatto";
 
-firstLetter = username.charAt(0);// Find char at position
-lastLetter = username.charAt(-1);// Last Letter
-firstOccurence = username.indexOf("a");// Find the first occurence
-lastOccurence = username.lastIndexOf("a");
-length = username.length;
-trimmedUsername = username.trim();
-UpperCase = trimmedUsername.toUpperCase();
-LowerCase = trimmedUsername.toLowerCase();
-StartsWith = username.startsWith("R");
-EndsWith = username.endsWith("f");
-Include = username.includes("@");
-replace = phonenumber.replaceAll(" ", "@");
+firstName = full_name.slice(0, full_name.indexOf(" "));
+middleName = full_name.slice(full_name.indexOf(" ") + 1, full_name.lastIndexOf(" "));
+lastName = full_name.slice(full_name.lastIndexOf(" ") + 1);
 
-console.log(replace)
+console.log(firstName);
+console.log(middleName);
+console.log(lastName);
+
+const email = "BroCode@gmail.com"
+user = email.slice(0, email.indexOf("@"));
+console.log(user);
