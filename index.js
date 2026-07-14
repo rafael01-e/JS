2626b73893521ce94d6721724255833c77d0eb1c
Rafael Ferreira Vasconcelos <rafaelferreiravasconcelos20@gmail.com>
2026-07-14 15:55:31 -0300

    Method chaining

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
-const full_name = "João Viscente Pagliatto";
+let username = window.prompt("Enter your username:");
 
-firstName = full_name.slice(0, full_name.indexOf(" "));
-middleName = full_name.slice(full_name.indexOf(" ") + 1, full_name.lastIndexOf(" "));
-lastName = full_name.slice(full_name.lastIndexOf(" ") + 1);
+username = username.trim().charAt(0).toUpperCase() + username.trim().slice(1).toLowerCase();
+console.log(username)
 
-console.log(firstName);
-console.log(middleName);
-console.log(lastName);
-
-const email = "BroCode@gmail.com"
-user = email.slice(0, email.indexOf("@"));
-console.log(user);
